447ae49c919b5cf75146d24915e90667037969c1
Ali Yıldırım <ge62fef@gitlab.lrz.de>
2025-01-26 21:22:36 +0100

    linting init

--- src/api/sensorRouter.py
+++ src/api/sensorRouter.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, Request
 from src.schemas.sensor_data import SensorData
 from src.services.sensor_service import SensorService
 
+
 class SensorRouter:
     def __init__(self):
         self.sensorService = SensorService()
@@ -12,48 +13,45 @@ class SensorRouter:
         self.router.post("/")(self.insertSensorData)
         self.router.get("/{gmlId}")(self.getByGmlId)
         self.router.get("/")(self.get)
-    
+
     async def insertSensorData(self, data: SensorData):
         result = self.sensorService.insertSensorData(data)
         return {
             "message": "Data inserted successfully",
             "inserted_id": result
         }
-        
+
     async def getByGmlId(self, gmlId: str):
         result = self.sensorService.getByGmlId(gmlId)
         if not result:
             raise HTTPException(status_code=404, detail=f"No sensor data found for gml_id: {gmlId}")
         return {
-                "message": "Data retrieved successfully",
-                "data": [
-                    {
-                        "id": data.id,
-                        "timestamp": data.timestamp,
-                        "gml_id": data.gml_id,
-                        "sensor_name": data.sensor_name,
-                        "value": data.value
-                    } for data in result
-                ]
-            }
+            "message": "Data retrieved successfully",
+            "data": [{
+                "id": data.id,
+                "timestamp": data.timestamp,
+                "gml_id": data.gml_id,
+                "sensor_name": data.sensor_name,
+                "value": data.value
+            } for data in result]
+        }
 
     async def get(self, request: Request):
         query_params = dict(request.query_params)
         result = self.sensorService.get(query_params)
         if not result:
-            raise HTTPException(status_code=404, detail=f"No sensor data found")
+            message = "No sensor data found"
+            raise HTTPException(status_code=404, detail=f"{message}")
         return {
-                "message": "Data retrieved successfully",
-                "data": [
-                    {
-                        "id": data.id,
-                        "timestamp": data.timestamp,
-                        "gml_id": data.gml_id,
-                        "sensor_name": data.sensor_name,
-                        "value": data.value
-                    } for data in result
-                ]
-            }
+            "message": "Data retrieved successfully",
+            "data": [{
+                "id": data.id,
+                "timestamp": data.timestamp,
+                "gml_id": data.gml_id,
+                "sensor_name": data.sensor_name,
+                "value": data.value
+            } for data in result
+            ]}
 
     def get_router(self):
         return self.router
--- src/db/models/sensor_reading.py
+++ src/db/models/sensor_reading.py
@@ -2,6 +2,7 @@ from sqlmodel import SQLModel, Field
 from datetime import datetime
 from typing import Optional
 
+
 class SensorReading(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     gml_id: str = Field(index=True)
