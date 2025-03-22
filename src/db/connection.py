fa2a46e271d013a2708d4b8d9d6538de007cdb3b
Ali Yıldırım <ge62fef@gitlab.lrz.de>
2025-03-22 18:09:03 +0100

    move db creations to sqlmodel and update the db models

--- src/db/connection.py
+++ src/db/connection.py
@@ -1,14 +1,14 @@
-from sqlmodel import SQLModel
 from src.core.db_config import timescale_engine, citydb_engine
+from src.db.bases import CityDBBase, TimescaleDBBase
 
 
 def init_db():
     try:
         print("Initializing TimescaleDB...")
-        SQLModel.metadata.create_all(timescale_engine)
+        TimescaleDBBase.metadata.create_all(timescale_engine)
 
         print("Initializing CityDB...")
-        SQLModel.metadata.create_all(citydb_engine)
+        CityDBBase.metadata.create_all(citydb_engine)
 
         print("Database initialization complete.")
     except Exception as e:
--- src/db/models/__init__.py
+++ src/db/models/__init__.py
@@ -1,3 +1,5 @@
 from .sensor_reading import SensorReading
+from .raster import Raster
+from .building2raster import Building2Raster
 
-__all__ = ["SensorReading"]
+__all__ = ["SensorReading", "Raster", "Building2Raster"]
