055da116514ba7e8ad9c0b53f126458dc024675e
Ali Yıldırım <ge62fef@gitlab.lrz.de>
2025-04-01 22:22:01 +0200

    api updates to write to db

--- src/db/models/sensor_reading.py
+++ src/db/models/sensor_reading.py
@@ -6,7 +6,7 @@ from src.db.bases import TimescaleDBBase
 
 class SensorReading(TimescaleDBBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    gml_id: str = Field(index=True)
+    raster_id: str = Field(index=True)
     timestamp: datetime
     sensor_name: str
     value: float
