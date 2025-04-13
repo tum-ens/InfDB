20230829012b5d6dbe45d8c1eb203baa5535aa72
Ali Yıldırım <ge62fef@gitlab.lrz.de>
2025-04-13 16:17:46 +0200

    example raster table creation to match with germany data

--- germany-grid.sql
+++ germany-grid.sql
@@ -1,19 +1,16 @@
-CREATE TABLE bkg_grid_10km AS
+CREATE TABLE ali AS
 SELECT
-  row_number() OVER () AS id,
+    '100kmN' || lpad((y / 100000)::text, 2, '0') || 'E' || lpad((x / 100000)::text, 2, '0') AS id,
   ST_SetSRID(
     ST_MakePolygon(
       ST_MakeLine(ARRAY[
         ST_MakePoint(x, y),
-        ST_MakePoint(x + 10000, y),
-        ST_MakePoint(x + 10000, y + 10000),
-        ST_MakePoint(x, y + 10000),
+        ST_MakePoint(x + 100000, y),
+        ST_MakePoint(x + 100000, y + 100000),
+        ST_MakePoint(x, y + 100000),
         ST_MakePoint(x, y)
       ])
     ), 3035
   ) AS geom
-FROM generate_series(2500000, 4600000 - 10000, 10000) AS x, -- START, END, STEPSIZE
-     generate_series(1400000, 3100000 - 10000, 10000) AS y; -- START, EBD, STEPSIZE
-
-
-     --- Easting: 340000, Northing: 6600000) to (Easting: 1220000, Northing: 7800000
+FROM generate_series(4000000, 4600000, 100000) AS x, -- START, END, STEPSIZE
+     generate_series(2600000, 3500000, 100000) AS y; -- START, EBD, STEPSIZE
