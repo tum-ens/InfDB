eac1be870e7a13c1651978de99f341ea99505a7f
Ali Yıldırım <ali.yldrm@tum.de>
2025-05-18 18:19:07 +0200

    Feature/use common env

--- data_import/imp/imp_basemap.py
+++ data_import/imp/imp_basemap.py
@@ -36,7 +36,7 @@ def import_basemap():
             print(f"Importing layer: {layer} into {schema}")
             gdf = gpd.read_file(file, layer=layer, bbox=gdf_envelope)
 
-            epsg = config.get_value(["database-data-import-container", "epsg"])
+            epsg = config.epsg
             gdf.to_crs(epsg=epsg, inplace=True)
 
             name = layer.replace("_bdlm", "")
