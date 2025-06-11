9b3f049c6284c4d79402695742e4c9dfe4b0c5cc
Ali Yıldırım <ge62fef@gitlab.lrz.de>
2025-06-11 20:26:37 +0300

    Optimize INFDB config and keep dockerfiles under service definitions

--- src/db/repositories/cityDbRepository.py
+++ src/db/repositories/cityDbRepository.py
@@ -1,6 +1,6 @@
 from sqlalchemy import text
 from sqlmodel import Session
-from src.core.dbConfig import citydb_engine
+from src.core.config import citydb_engine
 from fastapi import HTTPException
 
 
--- src/services/loader/sources/basemap.py
+++ src/services/loader/sources/basemap.py
@@ -1,31 +1,34 @@
 import os
 import geopandas as gpd
-
-from data_import.imp import config, utils
+from src.services.loader import utils
+from src.core import config
 
 
 def import_basemap():
+    status = config.get_value(["loader", "basemap", "status"])
+    if status != "active":
+        print("basemap skips, status not active")
+        return
 
-    # Create a database-data-import-container connection
     engine = utils.get_engine()
 
     # Get envelope
     sql = "SELECT geometry as geom FROM general.envelope"
     gdf_envelope = gpd.read_postgis(sql, engine)
 
-    base_path = config.get_path(["opendata", "basemap", "basemap_dir"])
-    processed_path = config.get_path(["opendata", "basemap", "basemap_processed_dir"])
+    base_path = config.get_path(["loader", "basemap", "basemap_dir"])
+    processed_path = config.get_path(["loader", "basemap", "basemap_processed_dir"])
     os.makedirs(base_path, exist_ok=True)
     os.makedirs(processed_path, exist_ok=True)
 
-    site_url = config.get_value(["opendata", "basemap", "url"])
-    ending = config.get_value(["opendata", "basemap", "ending"])
-    filter = config.get_value(["opendata", "basemap", "filter"])
+    site_url = config.get_value(["loader", "basemap", "url"])
+    ending = config.get_value(["loader", "basemap", "ending"])
+    filter = config.get_value(["loader", "basemap", "filter"])
     urls = utils.get_links(site_url, ending, filter)
 
     download_files = utils.download_files(urls, base_path)
 
-    schema = config.get_value(["opendata", "basemap", "schema"])
+    schema = config.get_value(["loader", "basemap", "schema"])
     # Create schema if it doesn't exist
     sql = f"CREATE SCHEMA IF NOT EXISTS {schema};"
     utils.sql_query(sql)
@@ -36,12 +39,9 @@ def import_basemap():
             print(f"Importing layer: {layer} into {schema}")
             gdf = gpd.read_file(file, layer=layer, bbox=gdf_envelope)
 
-            epsg = config.epsg
+            epsg = config.get_value(["services", "citydb", "epsg"])
             gdf.to_crs(epsg=epsg, inplace=True)
 
             name = layer.replace("_bdlm", "")
             gdf.to_postgis(name, engine, if_exists='append', schema=schema, index=False)
             gdf.to_file(os.path.join(processed_path, os.path.basename(file)), layer=name, driver="GPKG")
-
-
-import_basemap()
