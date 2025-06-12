3e438f23f143977fd8e0a7f20a0a3ea2b814bb3e
Ali Yıldırım <ge62fef@gitlab.lrz.de>
2025-06-12 23:09:45 +0300

    flake fix

--- src/services/sunpot/exporter.py
+++ src/services/sunpot/exporter.py
@@ -5,12 +5,12 @@ from sqlalchemy import create_engine, inspect, text
 from src.core import config
 
 load_dotenv()
-CITYDBV4_DB="citydb"
-CITYDBV4_USER="citydb_user"
-CITYDBV4_PASSWORD="citydb_password"
-CITYDBV4_EPSG=25832
-CITYDBV4_HOST="citydbv4"
-CITYDBV4_PORT="5432"
+CITYDBV4_DB = "citydb"
+CITYDBV4_USER = "citydb_user"
+CITYDBV4_PASSWORD = "citydb_password"
+CITYDBV4_EPSG = 25832
+CITYDBV4_HOST = "citydbv4"
+CITYDBV4_PORT = "5432"
 
 SCHEMA = "sunpot"
 OUTPUT_DIR = config.get_value(["base", "base_sunset_dir"])
