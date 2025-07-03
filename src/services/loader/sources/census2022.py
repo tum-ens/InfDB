7b2d68d1cc2b2158bd662d798b5137097d22f78c
Patrick Buchenberg <patrick.buchenberg@tum.de>
2025-07-03 10:56:42 +0200

    adjust loader structure

--- src/services/loader/sources/census2022.py
+++ src/services/loader/sources/census2022.py
@@ -4,14 +4,15 @@ import os
 from zipfile import ZipFile
 import geopandas as gpd
 import pandas as pd
-from src.services.loader import utils
+from src.services.loader import utils, logger
 from src.core import config
 
+log = logger.get_logger("infdb-loader")
 
 def get_zensus_links():
     # HTML-Seite herunterladen
 
-    url = config.get_value(["loader", "zensus_2022", "url"])
+    url = config.get_value(["loader", "sources", "zensus_2022", "url"])
 
     response = requests.get(url)
     html_content = response.content
@@ -32,69 +33,65 @@ def get_zensus_links():
 
     return zip_links
 
+# def download_zensus(zip_links):
+#
+#     os.makedirs(zip_path, exist_ok=True)
+#
+#     # Dateien herunterladen und entpacken
+#     for url in zip_links:
+#         # Dateiname aus URL extrahieren
+#         filename = os.path.join(zip_path, url.split("/")[-1])
+#         # print(f"Downloading {filename}")
+#         if os.path.exists(filename):
+#             print(f"File {filename} already exists.")
+#             continue
+#
+#         # Datei herunterladen
+#         response = requests.get(url)
+#         with open(filename, "wb") as file:
+#             file.write(response.content)
+#         log.info(f"{filename} downloaded.")
+#
+#     return zip_path
 
-def download_zensus(zip_links):
-    zip_path = config.get_path(["loader", "zensus_2022", "zensus_2022_zip_dir"])
-    os.makedirs(zip_path, exist_ok=True)
-
-    # Dateien herunterladen und entpacken
-    for url in zip_links:
-        # Dateiname aus URL extrahieren
-        filename = os.path.join(zip_path, url.split("/")[-1])
-        # print(f"Downloading {filename}")
-        if os.path.exists(filename):
-            print(f"File {filename} already exists.")
-            continue
-
-        # Datei herunterladen
-        response = requests.get(url)
-        with open(filename, "wb") as file:
-            file.write(response.content)
-        print(f"{filename} downloaded.")
-
-    return zip_path
-
-
-def unzip_zensus():
-    zip_path = config.get_path(["loader", "zensus_2022", "zensus_2022_zip_dir"])
-    unzip_path = config.get_path(["loader", "zensus_2022", "zensus_2022_unzip_dir"])
-    os.makedirs(unzip_path, exist_ok=True)
-
-    # Unzip downloaded files
-    zip_files = os.listdir(zip_path)
-    for zip_file in zip_files:
-        file_path = os.path.join(zip_path, zip_file)
-        # ZIP-Datei entpacken
-        with ZipFile(file_path, 'r') as zip_ref:
-            zip_ref.extractall(unzip_path)
-
-        print(f"{zip_file} unziped.")
+# def unzip_zensus():
+#     zip_path = config.get_path(["loader", "sources", "zensus_2022", "path", "zip"])
+#     unzip_path = config.get_path(["loader", "sources", "zensus_2022", "path", "unzip"])
+#     os.makedirs(unzip_path, exist_ok=True)
+#
+#     # Unzip downloaded files
+#     zip_files = os.listdir(zip_path)
+#     for zip_file in zip_files:
+#         file_path = os.path.join(zip_path, zip_file)
+#         # ZIP-Datei entpacken
+#         with ZipFile(file_path, 'r') as zip_ref:
+#             zip_ref.extractall(unzip_path)
+#
+#         log.info(f"{zip_file} unziped.")
 
 
 def process_census():
-    input_path = config.get_path(["loader", "zensus_2022", "zensus_2022_unzip_dir"])
-    output_path = config.get_path(["loader", "zensus_2022", "zensus_2022_processed_dir"])
-    processed_path = config.get_path(["loader", "bkg", "bkg_processed_dir"])
+    input_path = config.get_path(["loader", "sources", "zensus_2022", "path", "unzip"])
+    output_path = config.get_path(["loader", "sources", "zensus_2022", "path", "processed"])
+    processed_path = config.get_path(["loader", "sources", "zensus_2022", "path", "processed"])
 
     # path_root = "../../"
 
     os.makedirs(output_path, exist_ok=True)
 
     # Create schema if it doesn't exist
-    schema = config.get_value(["loader", "zensus_2022", "schema"])
-    sql = f"""
-        drop schema if exists {schema} cascade;
-        CREATE SCHEMA IF NOT EXISTS {schema};"""
+    schema = config.get_value(["loader", "sources", "zensus_2022", "schema"])
+    sql = f"CREATE SCHEMA IF NOT EXISTS {schema};"
     utils.sql_query(sql)
 
     # Create a database-data-import-container connection
-    engine = utils.get_engine()
+    engine = utils.get_db_engine("citydb")
 
     # Get envelope
-    sql = "SELECT geometry as geom FROM general.envelope"
-    gdf_envelope = gpd.read_postgis(sql, engine)
+    #sql = "SELECT geometry as geom FROM general.envelope"
+    gdf_envelope = utils.get_envelop()
 
-    resolutions = config.get_value(["loader", "zensus_2022", "resolutions"])
+    resolutions = config.get_value(["loader", "sources", "zensus_2022", "resolutions"])
     for resolution in resolutions:
 
         # Get grid within envelope
@@ -129,19 +126,27 @@ def process_census():
                 gdf_merged.to_file(os.path.join(output_path, f"zenus-2022{resolution}.gpkg"), layer=file, driver="GPKG")
 
             except Exception as err:
-                print(Exception, err)
-                print(file)
+                log.info(Exception, err)
+                log.info(file)
 
-    print("Zensus2022 imported successfully.")
+    log.info("Zensus2022 imported successfully.")
 
 
-def import_census2022():
-    status = config.get_value(["loader", "zensus_2022", "status"])
-    if status != "active":
-        print("zensus_2022 skips, status not active")
+def load():
+    if not utils.if_active("zensus_2022"):
+        log.info("zensus_2022 skips, status not active")
         return
 
     zip_links = get_zensus_links()
-    download_zensus(zip_links)
-    unzip_zensus()
+
+    #download_zensus(zip_links)
+    zip_path = config.get_path(["loader", "sources", "zensus_2022", "path", "zip"])
+    utils.download_files(zip_links, zip_path)
+
+    #zip_path = config.get_path(["loader", "sources", "zensus_2022", "path", "zip"])
+    unzip_path = config.get_path(["loader", "sources", "zensus_2022", "path", "unzip"])
+
+    zip_files = [os.path.join(zip_path, f) for f in os.listdir(zip_path)]
+    utils.unzip(zip_files, unzip_path)
+
     process_census()
