8b2380332a158c062aec1e3c2889d4c7ade50356
Patrick Buchenberg <patrick.buchenberg@tum.de>
2025-07-03 10:56:42 +0200

    adjust loader structure

---
+++ src/core/utils.py
@@ -0,0 +1,1 @@
+#import src.core.config as config
--- src/services/loader/startup.py
+++ src/services/loader/startup.py
@@ -1,16 +1,32 @@
+import multiprocessing as mp
 from src.services.loader.sources import basemap, bkg, census2022, lod2, plz
+import logger
 
-# Load LOD2 (building data)
-lod2.imp_lod2()
+logger.init_logger("infdb-loader", "infdb-loader.log")
 
-# Load BKG
-bkg.import_bkg()
+log = logger.get_logger("infdb-loader")
+log.info("Starting loader...")
 
-# Load Census2022
-census2022.import_census2022()
 
-# Load Zip Codes
-plz.import_plz()
+# Load NUTS Regions beforehand as basis
+#bkg.load()
+#basemap.load()
+#plz.load()
+#census2022.load()
+lod2.load()
 
-# Load Basemap
-basemap.import_basemap()
+# Load remaining data in parallel
+processes = []
+# processes.append(mp.Process(target=lod2.load()))
+# processes.append(mp.Process(target=census2022.load()))
+# processes.append(mp.Process(target=plz.load()))
+# processes.append(mp.Process(target=basemap.load()))
+
+# for process in processes:
+#     process.start()
+# print("Loader started")
+#
+# # Wait for processes
+# for process in processes:
+#     process.join()
+# print("Done")
