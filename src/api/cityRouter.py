8ab1e46ddad061235e85524ee4016d440077cdb4
Ali Yıldırım <ge62fef@gitlab.lrz.de>
2025-07-06 22:05:52 +0200

    flake fix

--- src/api/cityRouter.py
+++ src/api/cityRouter.py
@@ -28,7 +28,7 @@ class CityDbRouter:
         if not result:
             raise HTTPException(status_code=404, detail="No data found")
         return result
-    
+
     @router.get("/rasters/building/{building_id}", tags=["Citydb"])
     async def getRasterCenter(self, building_id: int, resolution: int = Query(...)):
         result = self.cityDbService.getRasterCenter(building_id, resolution)
