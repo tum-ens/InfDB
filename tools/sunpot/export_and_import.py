5e9ecedfdcadbcff42013e7356976b3bf0a528bd
Patrick Buchenberg <patrick.buchenberg@tum.de>
2025-07-30 10:14:32 +0200

    moved sunpot to from services to tools

--- tools/sunpot/export_and_import.py
+++ tools/sunpot/export_and_import.py
@@ -1,5 +1,5 @@
-from src.services.sunpot.exporter import export_to_csv
-from src.services.sunpot.importer import import_to_v5
+from tools.sunpot.exporter import export_to_csv
+from tools.sunpot.importer import import_to_v5
 
 print("exporting citydb v4 solar potential calculations to csv")
 export_to_csv()
