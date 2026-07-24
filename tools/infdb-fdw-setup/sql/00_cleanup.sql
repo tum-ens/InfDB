b79ab1cc45c884129a957b45c97aa50e68aba725
David Gantverg <>
2026-07-24 08:17:50 +0200

    Remove infdb-fdw-setup tool

--- tools/infdb-fdw-setup/sql/00_cleanup.sql
+++
@@ -1,7 +0,0 @@
---
--- Cleanup and initialize output schema
--- Drops the schema defined by {output_schema} if it exists, then creates a new empty schema.
--- Usage: Replaces {output_schema} with the target schema name defined by format_params before execution.
---
-DROP SCHEMA IF EXISTS {output_schema} CASCADE ;
-CREATE SCHEMA {output_schema};
